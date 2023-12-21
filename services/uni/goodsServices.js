cec40306994b5e04b315426f22b225939fa72f7a
LiuHaowei <2239827165@qq.com>
2023-12-21 17:49:13 +0800

    '完成商品热销、推荐'

--- services/uni/goodsServices.js
+++ services/uni/goodsServices.js
@@ -1,11 +1,13 @@
 const db = require('../../config/db.config')
 const { reServices } = require('../../hooks/goods')
 const goodsServices = {
-  getGoodsInfo: (req, res) => {
+  getGoodsInfo: async (req, res) => {
     const { id } = req.query
-    let sql = 'select * from goods where goods_id = ?'
-    let brand_id = null
-    db.executeQuery(sql, [id]).then(result => {
+    try {
+      // 查询商品信息
+      let sql = 'select * from goods where goods_id = ?'
+      let brand_id = null
+      const result = await db.executeQuery(sql, [id])
       brand_id = result[0].brand_id
       let goods_desc = result[0].goods_desc.split('.')
       let goods_albums = result[0].goods_albums.split(',')
@@ -16,16 +18,48 @@ const goodsServices = {
       } else {
         Object.assign(result[0], { goods_desc, goods_albums })
       }
+      //查询商品参数
       let s_sql = 'select parameter_name,parameter_value from goods_argument where goods_id = ?'
-      db.executeQuery(s_sql, ['10126']).then(argData => {
-        result[0].attrList = argData
-        let b_sql = 'select * from brand where brand_id = ?'
-        db.executeQuery(b_sql, [brand_id]).then(brandData => {
-          result[0].brand_info = brandData[0]
-          res.send(result[0])
-        })
+      const argData = await db.executeQuery(s_sql, ['10126'])
+      result[0].attrList = argData
+
+      //查询商品品牌信息
+      let b_sql = 'select * from brand where brand_id = ?'
+      const brandData = await db.executeQuery(b_sql, [brand_id])
+      result[0].brand_info = brandData[0]
+
+      //查询相关商品信息
+      // let c_sql = 'select parent_id from goods_category where category_id = ?'
+      let sqls = 'select category_id from goods_category where parent_id = ?'
+      // const [{ parent_id }] = await db.executeQuery(c_sql, [result[0].category_id])
+      let ids = await db.executeQuery(sqls, [37])
+      // let ids = await db.executeQuery(sqls, [parent_id])
+      let promise = []
+      let c_sql_ = 'select goods_id,goods_name,goods_price,retail_price,goods_img from goods where category_id = ?'
+      ids.forEach(item => promise.push(db.executeQuery(c_sql_, [item.category_id])))
+      const aboutGoods = await Promise.all(promise)
+      let list = new Array(4).fill().map(() => [])
+      list.forEach((item, index) => item.push(...aboutGoods.flat().slice(index * 6, (index + 1) * 6)))
+      result[0].aboutGoods = list
+
+      //查询24小时热销商品信息
+      let h_sql = 'select category_id from goods_category where parent_id = ?'
+      let c_ids = await db.executeQuery(h_sql, [289])
+      let promises = []
+      c_ids.forEach(item => promises.push(db.executeQuery(c_sql_, [item.category_id])))
+      const hotGoods = await Promise.all(promises)
+      let h_list = new Array(4).fill().map(() => [])
+      h_list.forEach((item, index) => item.push(...hotGoods.flat().slice(index * 6, (index + 1) * 6)))
+      result[0].hotGoods = h_list
+
+      //响应结果
+      res.status(200).json({
+        result: result[0],
+        msg: 'success'
       })
-    })
+    } catch (error) {
+      res.status(500).json({ success: false, message: 'Internal Server Error.' })
+    }
   },
   getBrandGoods: (req, res) => {
     // let { brand_id } = req.query
